0c724e98fe9a739cd981709dc2a996066da28ae5
cassxw <cassandra.s.wallace@gmail.com>
2025-04-12 22:10:09 +0200

    Solved: 104_Maximum-Depth-of-Binary-Tree, for Top Interview 150

---
+++ Topics/Binary-Tree/104_Maximum-Depth-of-Binary-Tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        // Binary Tree (DFS, BFS)
+
+        // root = The root of a binary tree.
+
+        // Return the tree's maximum depth.
+        // i.e. the number of nodes along the longest path from
+        //      the root node down to the farthest leaf node.
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(
+            maxDepth(root.left),
+            maxDepth(root.right)
+        );
+    }
+}
